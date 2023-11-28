1286f75dbde6257f7409d663c093d66eb21792e2
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-28 16:13:40 +0100

    fix: function js with start game button "click" also  img logo

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 const layout= document.getElementById("grid");
 
 const caselle=100;
-for(let i =1; i<=100;i++){
-    const div= document.createElement("div");
-    div.classList.add("square");
-    div.append(i);
-    div.addEventListener("click",
-        function(){
-            div.style.background="#7FFFD4";
-        }
-    )
-    layout.append(div);
 
+function start_game(){
+    for(let i=1; i<=caselle;i++){
+        const div= document.createElement("div");
+        div.classList.add("square");
+        div.append(i);
+        div.addEventListener("click",
+            function(){
+                div.style.background="#7FFFD4";
+            }
+        )
+        layout.append(div);
+    
+    }
 }
+
+
+// FUNZIONE CHE GENERA I DIV CON QUALSIASI CLASSE .
