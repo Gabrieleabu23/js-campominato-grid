43904f990af26fbf84aea6c04e89d2cbfc71e768
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-28 18:48:24 +0100

    fix: footer, classlist clicked and js script add

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 const layout= document.getElementById("grid");
-
+layout.classList.add("d-none");
 const caselle=100;
-
 function start_game(){
+    document.getElementById("main_container").classList.add("pt-4","pb-4");
+    document.querySelector("footer").classList.remove("position-fixed");
+    layout.classList.remove("d-none");
+    document.getElementById("grid").innerHTML=" ";
     for(let i=1; i<=caselle;i++){
-        const div= document.createElement("div");
-        div.classList.add("square");
+        const div= generateElements("div","square");
         div.append(i);
-        div.addEventListener("click",
-            function(){
-                div.style.background="#7FFFD4";
-            }
-        )
+        onlick_azzurrino(div,i);
         layout.append(div);
     
     }
@@ -19,3 +17,20 @@ function start_game(){
 
 
 // FUNZIONE CHE GENERA I DIV CON QUALSIASI CLASSE .
+
+function generateElements(tagtype,classname){
+    const currentElement= document.createElement(tagtype);
+    currentElement.classList.add(classname);
+    return currentElement;
+}
+
+// FUNZIONE CHE QUANDO CLICCHI IL COLORE DIVENTA AZZURRINO
+
+function onlick_azzurrino(div,i) {
+    div.addEventListener("click",
+        function(){
+            div.classList.add("clicked");
+            console.log("Cella cliccata: ",i);
+        }
+    )
+  }
